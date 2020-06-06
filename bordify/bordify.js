91a07019cb95fe70c6dd75b69a715b2ee2e34f23
wbne <firetruckbw@gmail.com>
2020-06-06 14:20:52 -0500

    made the popup thingy
    now it shows an icon that lists stuff.
    there's a lot of things I need to add though...

--- bordify/bordify.js
+++ bordify/bordify.js
@@ -1,9 +1,3 @@
-document.body.style.border = "5px solid red";
-
-const blob = document.body.createElement('div')
-blob.setAttribute('id', 'time')
-blob.textContent("help me")
-
 window.onload = function () {
-
+    setTimeout(function(){ alert("Hello"); }, 3000);
 };
