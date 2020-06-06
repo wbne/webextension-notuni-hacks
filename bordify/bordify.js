7b0712131d5166325dfc44bc81cde46a9a4a1665
wbne <firetruckbw@gmail.com>
2020-06-06 12:51:31 -0500

    made an extension
    it kinda jank tho

---
+++ bordify/bordify.js
@@ -0,0 +1,9 @@
+document.body.style.border = "5px solid red";
+
+const blob = document.body.createElement('div')
+blob.setAttribute('id', 'time')
+blob.textContent("help me")
+
+window.onload = function () {
+
+};
